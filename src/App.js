4ecace1c0dddb74706e9d19d03199c0015af0407
Ramon Larivoir <rlarivoir@hotmail.com>
2021-05-25 17:50:42 -0300

    Adicionando o board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Client } from 'boardgame.io/react';
+import { TicTacToeBoard } from './Board';
 import { TicTacToe } from './Game';
 
 
-const App = Client({ game: TicTacToe });
+const App = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard
+});
 
 export default App;
