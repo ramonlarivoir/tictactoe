c7f1b9e5bf16938115b3ee6495aad833a963d397
Ramon Larivoir <rlarivoir@hotmail.com>
2021-05-25 17:29:43 -0300

    Adicionando gerenciamento de turnos

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,30 @@
 import { INVALID_MOVE } from 'boardgame.io/core';
 /*
-'setup' é uma função que inicializa os valores do estado do jogo (game state) 'G'
-'G' e 'ctx' são objetos gerenciados pelo próprio boardgame.io
-'ctx' contem metadata como 'turn' e 'currentPlayer'
+  'setup' é uma função que inicializa os valores do estado do jogo (game state) 'G'
+  'G' e 'ctx' são objetos gerenciados pelo próprio boardgame.io
+  'ctx' contem metadata como 'turn' e 'currentPlayer'
 */
 
 export const TicTacToe = {
-  // Cria as células com 9 posições
+  //  Cria as células com 9 posições
   setup: () => ({ cells: Array(9).fill(null) }),
+
+  /*  
+    Definindo a quantidade de movimentos do player em cada turno.
+    Ao finalizar o movimento, o turno finaliza. 
+  */
+  turn: {
+    moveLimit: 1,
+  },
 
   moves: {
     clickCell: (G, ctx, id) => {
-      // Valida se a célula já está em uso
+      //  Valida se a célula já está em uso
       if (G.cells[id] !== null) {
         return INVALID_MOVE;
       }
-      // Seta o id da célula com o id do player atual
+
+      //  Seta o id da célula com o id do player atual
       G.cells[id] = ctx.currentPlayer;
     },
   },
